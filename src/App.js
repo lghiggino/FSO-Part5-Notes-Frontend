c945b7fe43569676dc9c1a510d4fde08295d6e26
lghiggino <lghiggino@gmail.com>
2022-11-26 00:14:35 -0300

    transfer my part2 into part5

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ const App = () => {
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [username, setUsername] = useState('') 
-  const [password, setPassword] = useState('') 
 
   useEffect(() => {
     noteService
@@ -20,11 +18,6 @@ const App = () => {
         setNotes(initialNotes)
       })
   }, [])
-
-  const handleLogin = (event) => {
-    event.preventDefault()
-    console.log('logging in with', username, password)
-  }
 
   const addNote = (event) => {
     event.preventDefault()
@@ -75,30 +68,6 @@ const App = () => {
     <div>
       <h1>Notes</h1>
       <Notification message={errorMessage} />
-
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all' }
