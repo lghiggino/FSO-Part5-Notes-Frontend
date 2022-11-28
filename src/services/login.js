dca740004781be31382bacdea3963af0fa0ee10f
lghiggino <lghiggino@gmail.com>
2022-11-28 06:54:34 -0300

    loginService

---
+++ src/services/login.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const baseUrl =
+  process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test"
+    ? "http://localhost:3001/api/login"
+    : "/api/login";
+
+const login = async (credentials) => {
+  const response = await axios.post(baseUrl, credentials);
+  return response.data;
+};
+
+export default { login };
