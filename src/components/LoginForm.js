51d2297e9ab5dfc7ff1b93a3046f10a6f8724e76
lghiggino <lghiggino@gmail.com>
2022-11-29 07:54:23 -0300

    refactor LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,29 +1,49 @@
-export const LoginForm = ({
-  handleLogin,
-  username,
-  setUsername,
-  password,
-  setPassword,
-}) => (
-  <form onSubmit={handleLogin} style={{ marginBottom: 20 }}>
-    <div>
-      username
-      <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
-      <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
-);
+import { useState } from "react";
+import loginService from "../services/login";
+import noteService from "../services/notes";
+
+export const LoginForm = ({ setUser, setErrorMessage }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+      noteService.setToken(user.token);
+      noteService.setUserId(user.token)
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      setErrorMessage("Wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+  return (
+    <form onSubmit={handleLogin} style={{ marginBottom: 20 }}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+};
