baab52283059d278332ed8cc9b4fe6c19147d3c5
lghiggino <lghiggino@gmail.com>
2023-01-16 08:52:45 -0300

    Note app test clears testDB, and passes

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Notes app
 
 READ HERE: https://dmitripavlutin.com/react-useref-guide/
+
+//https://fullstackopen.com/en/part5/end_to_end_testing#controlling-the-state-of-the-database
--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe("Note app", function () {
   beforeEach(function () {
+    cy.request("POST", "http://localhost:3001/api/testing/reset");
+    const user = {
+      name: "Matti Luukkainen",
+      username: "mluukkai",
+      password: "salainen",
+    };
+    cy.request("POST", "http://localhost:3001/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
@@ -16,7 +23,7 @@ describe("Note app", function () {
     cy.get("#input-password").type("salainen");
     cy.get("#button-login").click();
 
-    cy.contains("Matti Luukkaineno logged-in");
+    cy.contains("Matti Luukkainen logged-in");
   });
 
   localStorage.removeItem("loggedNoteappUser");
@@ -28,7 +35,7 @@ describe("Note app", function () {
       cy.get("#input-password").type("salainen");
       cy.get("#button-login").click();
 
-      cy.contains("Matti Luukkaineno logged-in");
+      cy.contains("Matti Luukkainen logged-in");
     });
 
     it("a new note can be created", function () {
